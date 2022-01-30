eaf4edf4cf0ec67ed73b08f620d861d99e969908
Benjamin7222 <benjidu722@gmail.com>
2022-01-30 14:23:58 -0800

    rre

---
+++ [Maps]/[mplibertycity]/mplibertycity_garbage/__resource.lua
@@ -0,0 +1,17 @@
+this_is_a_map "yes"
+
+data_file "INTERIOR_PROXY_ORDER_FILE" "interiorproxies.meta"
+
+files {
+	"interiorproxies.meta",
+        "stream/lastfixes/v_ng_proppatch_01.ytyp",
+         "stream/[gtav_overhandle_int]/v_int_38/v_int_38.ytyp",
+         "stream/[gtav_overhandle_int]/hei_heist_ornate_bank/hei_heist_ornate_bank.ytyp",
+         "stream/[gtav_overhandle_int]/v_carmod/v_int_46.ytyp"
+}
+
+
+data_file "PERMANENT_ITYP_REQUEST" "stream/[gtav_overhandle_int]/hei_heist_ornate_bank/hei_heist_ornate_bank.ytyp"
+data_file "PERMANENT_ITYP_REQUEST" "stream/[gtav_overhandle_int]/v_carmod/v_int_46.ytyp"
+
+
