db77cd77c719bef2e4bf966afb1c74df27b35544
Benjamin7222 <benjidu722@gmail.com>
2022-01-30 14:26:43 -0800

    trrtr

---
+++ [Maps]/[mplibertycity]/mplibertycity_assets/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+games { 'gta5' };
+
+files {
+    "carcols.meta",
+    "carvariations.meta",
+    "carvariations_lc.meta",
+    "dlctext.meta",
+    "handling.meta",
+    "vehicles.meta",
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations_lc.meta'
